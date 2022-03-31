4f644dbd3d147f6a7f68c18853e5e9ad43800283
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-31 15:45:00 +0200

    converted to handlebars

--- assignments/Shnert/app.js
+++ assignments/Shnert/app.js
@@ -1,10 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
+import express from 'express';
+import { engine } from 'express-handlebars';
+import bodyParser from "body-parser";
+import fs from 'fs';
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/public/views'));
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Hierdoor kan ik CSS en JS bestanden aan de client side uitlezen
+app.use(express.static('public'));
+
+// Gebruik template engine handlebars voor DYNAMISCHE content
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', 'public/views');
+
+// Gebruik body-parser om te lezen wat er in POST requests van de form staat
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', function (req, res) {
     res.render('home', {
